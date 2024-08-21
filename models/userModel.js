bcafd80541d97e0b67584f4a24a5d3ac4ae3dda1
Faisal Karim Chowdhury <faisalchy64@gmail.com>
2024-08-21 14:42:54 +0600

    feat: Create userModel.js model

---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+    role: {
+      type: String,
+      required: [true, "Role is required."],
+      enum: ["user", "supplier"],
+      default: "user",
+    },
+    address: {
+      type: String,
+      default: "",
+    },
+    refreshToken: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
